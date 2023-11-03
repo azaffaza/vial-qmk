95f9ac633ebd11cd0f190b03eb9ce19ebb7639ce
azaffaza <abizargi@gmail.com>
2023-11-03 21:34:41 +0700

    Some changes in sicnine & blankspace

---
+++ keyboards/azafactory/sicnine/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xA4, 0x7D, 0x26, 0xBC, 0xFC, 0x36, 0x32, 0x58}
+
+/* CONSIDER ADDING AN UNLOCK COMBO. SEE DOCUMENTATION. */
+#define VIAL_INSECURE
