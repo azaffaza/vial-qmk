b5d078311bf5c6b070441cc5dc7e226af35dc6ef
azaffaza <abizargi@gmail.com>
2023-11-04 11:05:18 +0700

    Update sicpad firmware

---
+++ keyboards/azafactory/sicpad/keymaps/vial/config.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xDD, 0xB4, 0xE1, 0xD7, 0x00, 0x1D, 0x5C, 0x93}
+
+/* CONSIDER ADDING AN UNLOCK COMBO. SEE DOCUMENTATION. */
+#define VIAL_INSECURE
+#define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+#define RGB_MATRIX_KEYPRESSES
