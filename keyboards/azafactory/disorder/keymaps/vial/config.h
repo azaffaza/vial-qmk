38c94dff1e6a34adeb7e1e1b8bd1bad7ff507d6c
azaffaza <abizargi@gmail.com>
2023-11-07 01:39:44 +0700

    Added disorder firmware

---
+++ keyboards/azafactory/disorder/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xFC, 0x3A, 0x80, 0x7E, 0x53, 0x87, 0x97, 0x65}
+
+/* CONSIDER ADDING AN UNLOCK COMBO. SEE DOCUMENTATION. */
+#define VIAL_INSECURE
