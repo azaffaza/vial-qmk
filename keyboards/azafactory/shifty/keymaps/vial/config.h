2189e007a666dbfd55eda23914685fff0edf293c
azaffaza <abizargi@gmail.com>
2023-11-14 20:38:31 +0700

    Scrap shifty

--- keyboards/azafactory/shifty/keymaps/vial/config.h
+++
@@ -1,8 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0-or-later */
-
-#pragma once
-
-#define VIAL_KEYBOARD_UID {0x5B, 0x9B, 0x3B, 0x92, 0xCF, 0x6F, 0x0A, 0xCD}
-
-/* CONSIDER ADDING AN UNLOCK COMBO. SEE DOCUMENTATION. */
-#define VIAL_INSECURE
