6172fe8b6ab3d2fc73b09379336e0d0fc99e5663
azaffaza <abizargi@gmail.com>
2023-11-19 19:48:05 +0700

    Update dividuus

--- keyboards/azafactory/dividuus/config.h
+++ keyboards/azafactory/dividuus/config.h
@@ -5,10 +5,11 @@
 #define DYNAMIC_KEYMAP_LAYER_COUNT 3
 #define NO_ACTION_ONESHOT
 #define FORCE_NKRO
+#define EE_HANDS
 
 #define SERIAL_USART_FULL_DUPLEX   // Enable full duplex operation mode.
 #define SERIAL_USART_TX_PIN A2     // USART TX pin
 #define SERIAL_USART_RX_PIN A3     // USART RX pin
 #define SERIAL_USART_PIN_SWAP      // Swap TX and RX pins if keyboard is master halve. (Only available on some MCUs)
 #define SERIAL_USART_TX_PAL_MODE 1 // Pin "alternate function", see the respective datasheet for the appropriate values for your MCU. default: 7
- #define SERIAL_USART_DRIVER SD2
+#define SERIAL_USART_DRIVER SD2
