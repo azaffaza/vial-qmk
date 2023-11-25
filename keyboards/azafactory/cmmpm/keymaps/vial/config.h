ffe4343a2b1ad1ded070259510eaea30507f38c2
azaffaza <abizargi@gmail.com>
2023-11-25 20:29:53 +0700

    Update cmmpm

---
+++ keyboards/azafactory/cmmpm/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x96, 0xA6, 0x24, 0x24, 0x00, 0xDD, 0x4D, 0x3C}
+
+/* CONSIDER ADDING AN UNLOCK COMBO. SEE DOCUMENTATION. */
+#define VIAL_INSECURE
---
+++ keyboards/azafactory/cmmpm/mcuconf.h
@@ -0,0 +1,8 @@
+#pragma once
+#include_next <mcuconf.h>
+
+#undef STM32_I2C_USE_I2C1
+#define STM32_I2C_USE_I2C1 TRUE
+
+#undef STM32_PWM_USE_TIM1
+#define STM32_PWM_USE_TIM1 TRUE
