1625de9f9cc85a73e9849c4ab7055e9d0e1f9502
azaffaza <abizargi@gmail.com>
2023-11-30 08:33:52 +0700

    Added keymap_steno.h (akhirnya compile)

--- keyboards/azafactory/wpm_3000/keymaps/default/keymap.c
+++ keyboards/azafactory/wpm_3000/keymaps/default/keymap.c
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
 
 #include QMK_KEYBOARD_H
+#include "keymap_steno.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
--- keyboards/azafactory/wpm_3000/keymaps/vial/keymap.c
+++ keyboards/azafactory/wpm_3000/keymaps/vial/keymap.c
@@ -1,17 +1,14 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
 
 #include QMK_KEYBOARD_H
-
-enum layers {
-    _PLOVER
-};
+#include "keymap_steno.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-	[_PLOVER] = LAYOUT(
-		STN_SL, STN_TL, STN_PL, STN_HL, STN_STR, STN_FR, STN_PR, STN_LR, STN_TR, STN_DR,
-		        STN_KL, STN_WL, STN_RL,          STN_RR, STN_BR, STN_GR, STN_SR, STN_ZR,
-		                STN_A,  STN_O,           STN_E,  STN_U
+	[0] = LAYOUT(
+	STN_SL, STN_TL, STN_PL, STN_HL, STN_STR, STN_FR, STN_PR, STN_LR, STN_TR, STN_DR,
+	    	STN_KL, STN_WL, STN_RL,          STN_RR, STN_BR, STN_GR, STN_SR, STN_ZR,
+	            	STN_A,  STN_O,           STN_E,  STN_U
 	),
 
 };
