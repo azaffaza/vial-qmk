2a32d9667c7c0df48e09be7d0d9becf187985a6d
azaffaza <abizargi@gmail.com>
2023-11-30 11:38:16 +0700

    Delete vial

--- keyboards/azafactory/wpm_3000/keymaps/vial/config.h
+++
@@ -1,8 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0-or-later */
-
-#pragma once
-
-#define VIAL_KEYBOARD_UID {0x3C, 0xAA, 0x72, 0x12, 0xFF, 0x91, 0xE4, 0x55}
-
-/* CONSIDER ADDING AN UNLOCK COMBO. SEE DOCUMENTATION. */
-#define VIAL_INSECURE
